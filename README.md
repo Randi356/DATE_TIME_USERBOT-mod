9bb5cc41784c5c10372e8bbfd5c75b4faf1a24ba
Randy Wahidin <63757267+Randi356@users.noreply.github.com>
2021-10-05 14:22:17 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,47 @@
+<h1 align= center>DATE_TIME_USERBOT-MOD</h1>
+<h3 align = center>Powerful Telegram userbot to turn your PROFILE PICTURE & LAST NAME into a real time clock & to change your BIO automatically.</h3>
+<p align="center">
+<a href="https://python.org"><img src="http://forthebadge.com/images/badges/made-with-python.svg" alt="made-with-python"></a>
+<br>
+    <img src="https://img.shields.io/github/stars/Randi356/DATE_TIME_USERBOT-mod?style=for-the-badge" alt="Stars">
+    <img src="https://img.shields.io/github/forks/Randi356/DATE_TIME_USERBOT-mod?style=for-the-badge" alt="Forks">
+    <img src="https://img.shields.io/github/watchers/Randi356/DATE_TIME_USERBOT-mod?style=for-the-badge" alt="Watchers"> 
+<br>
+    <img src="https://img.shields.io/github/license/Randi356/DATE_TIME_USERBOT-mod?style=for-the-badge" alt="License">
+    <img src="https://img.shields.io/github/repo-size/Randi356/DATE_TIME_USERBOT-mod?style=for-the-badge" alt="Repository Size">
+    <img src="https://img.shields.io/github/contributors/Randi356/DATE_TIME_USERBOT-mod?style=for-the-badge" alt="Contributors">
+    <img src="https://img.shields.io/github/issues/Randi356/DATE_TIME_USERBOT-mod?style=for-the-badge" alt="Issues">
+</p>  
+
+## Config Vars
+1. `API_ID` : Telegram API_ID, get it from my.telegram.org/apps
+2. `API_HASH` : Telegram API_ID, get it from my.telegram.org/apps
+3. `SESSION_NAME` : A valid Pyrogram session name
+4. `TIME_ZONE`: Your time zone (Example: Asia/Jakarta)
+
+<p align="center">
+<img src="https://telegra.ph/file/577fd708b51a9c60053f7.jpg">
+<p>
+ 
+## Deployment Methods
+
+### Heroku
+
+[![Deploy](https://www.herokucdn.com/deploy/button.svg)](https://heroku.com/deploy?template=https://github.com/Randi356/DATE_TIME_USERBOT-mod)
+    
+## Credits
+- [Xsvshacker](https://github.com/Randi356)
+- [Thakshaka](https://t.me/thakshakar)
+- [Pyrogram](https://github.com/pyrogram/pyrogram)
+
+## ⚠️ First & Last Warning
+
+- Changing the code is NOT ALLOWED!!    
+- You are free to use this code in any of your projects, but you MUST include the following in your README.md (Copy & paste)
+- [📞 Contact](https://t.me/FlashProSpeed)
+```
+##Credits
+- [DATE_TIME Telegram userbot by xsvshacker] (https://github.com/Randi356/CountdownTimer-Randi356)
+```
+## Read GNU AFFERO GENERAL PUBLIC LICENSE: 
+[LICENSE](https://github.com/Randi356/DATE_TIME_USERBOT-mod/blob/main/LICENSE)
---
+++ date_time_userbot_mod.py
@@ -0,0 +1,58 @@
+#Copyright ©️ 2021 xsvshacker. All Rights Reserved
+#You are free to use this code in any of your project, but you MUST include the following in your README.md (Copy & paste)
+# ##Credits - [DATE_TIME Telegram userbot by xsvshacker] (https://github.com/Randi356/DATE_TIME_USERBOT-mod)
+
+# Changing the code is not allowed! Read GNU AFFERO GENERAL PUBLIC LICENSE: https://github.com/Randi356/DATE_TIME_USERBOT-mod/blob/main/LICENSE
+
+from pyrogram import Client, filters
+from pyrogram.errors import FloodWait
+from lists_mod.quotes_mod import *
+from lists_mod.emojis_mod import *
+from PIL import Image, ImageDraw, ImageFont
+import datetime
+import pytz
+import asyncio
+import random
+import os
+
+Date_Time_Userbot_mod=Client(
+    api_id = int(os.environ["API_ID"]),
+    api_hash = os.environ["API_HASH"],
+    session_name = os.environ["SESSION_NAME"]
+)
+
+Time_Zone = os.environ["TIME_ZONE"]
+
+async def main_mod():
+    try:
+        while True:
+            if Date_Time_Userbot_mod.is_connected:
+                Quotes_mod = random.choice(quotes_mod)
+                Emojis_mod = random.choice(emojis_mod)
+                TimeZone_mod = datetime.datetime.now(pytz.timezone(f"{Time_Zone}"))
+                Time_mod = TimeZone_mod.strftime("%I:%M %p")
+                Date_mod = TimeZone_mod.strftime("%b %d") 
+                Image_mod = Image.open("image.jpg")
+                Image_font_mod = ImageFont.truetype("ds-digit.ttf", 360)
+                Image_text_mod = f"{Time_mod}"
+                Image_edit_mod = ImageDraw.Draw(Image_mod)
+                Image_edit_mod.text((690, 550), Image_text_mod, (0, 255, 255), font = Image_font_mod)
+                Image_mod.save("Image_final_mod.jpg")
+                await Date_Time_Userbot_mod.update_profile(bio = f"{Emojis_mod} {Quotes_mod}" , last_name = f"| ⏰ {Time_mod} | 📅 {Date_mod}")
+                await Date_Time_Userbot_mod.set_profile_photo(photo="Image_final_mod.jpg")
+                me = await Date_Time_Userbot_mod.get_me()
+                photos = await Date_Time_Userbot_mod.get_profile_photos("me")
+                try:
+                    await Date_Time_Userbot_mod.delete_profile_photos(photos[1].file_id)
+                except Exception:
+                    pass        
+                print("Profile Updated!")
+            await asyncio.sleep(60)     
+    except FloodWait as e:
+        await asyncio.sleep(e.x)         
+
+print("DATE TIME USERBOT IS ALIVE!")
+asyncio.ensure_future(main_mod())
+Date_Time_Userbot_mod.run()
+
+#Copyright ©️ 2021 xsvshacker. All Rights Reserved
